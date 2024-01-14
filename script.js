5869b5a814a6c73457a5e2ec0f538d20b2be1330
hieppham02 <83917749+hieppham02@users.noreply.github.com>
2024-01-15 03:09:56 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -7,12 +7,12 @@ function OpenLeague(leagueName) {
   document.getElementById(leagueName).style.display = "table";
 }
 function OpenPlayer(leaguePlayer) {
-    var i;
-    var x = document.getElementsByClassName("league-player");
-    for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none";
-    }
-    document.getElementById(leaguePlayer).style.display = "table";
+  var i;
+  var x = document.getElementsByClassName("league-player");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  document.getElementById(leaguePlayer).style.display = "table";
 }
 function OpenDescription(leagueDes) {
   var i;
@@ -37,3 +37,14 @@ function ClickChangeColor_3() {
   document.getElementById("btn-l2").style.color = "black";
   document.getElementById("btn-l3").style.color = "#00cc47";
 }
+document.addEventListener("DOMContentLoaded", function () {
+  // Trích xuất mảng HTML từ localStorage
+  var storedHtmlArray =
+    JSON.parse(localStorage.getItem("teamObjectArray")) || [];
+
+  // Hiển thị HTML trong thẻ insert-html
+  if (storedHtmlArray.length > 0) {
+    var htmlToShow = storedHtmlArray.map((item) => item.html).join("");
+    document.getElementById("insert-html").innerHTML = htmlToShow;
+  }
+});
